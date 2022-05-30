93b925cb6bcb11f1bf052db79f05dd9fb94c23ec
Otávio Monteiro <otavio.monteiro@flashapp.com.br>
2022-05-30 16:12:11 -0300

    Esqueleto EP2

---
+++ EP2/ep2.cpp
@@ -0,0 +1,52 @@
+//EP2 MAP3121
+//Diogo Vaccaro 8803195
+//Otávio Henrique Monteiro 10774159
+
+#include<stdio.h>
+#include <iostream>
+#include <cmath>
+
+#define MAX 10
+
+using namespace std;
+
+
+int main(){
+    // INICIALIZAR VARIÁVEIS
+    // Nº de nós
+    int n = 6;
+    //Nós
+    double x[MAX];
+    //Pesos
+    double w[MAX];
+
+
+
+
+
+    cout<< "-- Bem vindo ao EP --"<< endl;
+        //Por terminal 
+    //cout<< "Digite o numero 'n' de nós da integração: ";
+    //cin>> n;
+        //Por arquivo
+    
+    
+
+    // SOLUCAO
+    
+
+
+    // Imprimir resposta
+    cout << "Para n igual a " << n <<endl;
+    cout << "O resultado da Integração é:" << endl;
+    
+    //Calcular resultado exato
+    
+    
+    //Finalizar
+    cout << "\n\nDigite algum caractere para finalizar.\n";
+    char end;
+    cin >> end;
+    return 1;
+}
+
