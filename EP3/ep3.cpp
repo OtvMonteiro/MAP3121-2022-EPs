7f27595526b9a24d63d05d098b7b436782338c8f
OtavioHMonteiro <otavio.monteiro@flashapp.com.br>
2022-07-13 22:00:21 -0300

    Validacao feita

--- EP3/ep3.cpp
+++ EP3/ep3.cpp
@@ -14,7 +14,7 @@ const double ks = 3.6; // Condutividade Termica do Silicio W/mK
 const double ka = 60;  // Condutividade Termica do Aluminio W/mK
 // Variaveis globais de execucao
 char questao = '1'; //'t'; // Variavel universal para escolha de questao
-bool debug = true;
+bool debug = false; // Quando true imprime diversos valores intermediarios
 // fronteira
 double fa = 0.0;
 double fb = 0.0;
@@ -25,7 +25,6 @@ double P = 30;     // W
 
 using namespace std;
 
-double calcula_integral(double a, double b, double T);
 double integra(double a, double b, double xi, double h, double (*func)(double, double, double));
 double phi_i(double h, double x, double xi_ant, double xi, double xi_prox);
 double k(double x);
@@ -46,7 +45,7 @@ int main()
     //  Parametros Gauss (n=2, w=1)
     double T = 1 / sqrt(3); // Abcissa 2 pontos (uma negativa e outra positiva)
     // Matriz Tridiagonal
-    int n; // Dimensao da matriz
+    int _n[] = {7, 15, 31, 63}; // Dimensao da matriz
     double a[MAX];
     double b[MAX];
     double c[MAX];
@@ -59,89 +58,97 @@ int main()
     cout.precision(17);
 
     cout << "Bem-Vindo ao EP3 de MAP3121-2022 \n";
-    cout << "Digite o numero n de linhas e colunas da matriz Anxn: ";
-    cin >> n;
+    // cout << "Digite o numero n de linhas e colunas da matriz Anxn: ";
+    // cin >> n;
 
     // EXECUCAO
-    // Montagem da matriz com produtos internos (integrais)
-    double h;
-    if (questao == 't')
-        h = 1 / ((double)n + 1);
-    else
-        h = L / ((double)n + 1); // Intervalo de [0, L] // TODO: corrigir para esse caso
-                                 // Na validacao q=0 e k=1
-                                 // ~f = f +(b-a)*k' - q*(a + (b-a)*x)
-
-    // NOTE: IMPORTANTE TODO: no livro ele faz 6 tipos de integracao, que o pdf nao fala (tem tamb simplificacao, mas sem integracao),
-    //   precisa entender o que exatamente ele quer aqui, se e' a equacao abiaxo de (6) pra usarmos q(x), se conseguimos fazer so' mais os produtos de phi (e ai considerar o q(x))...
-
-    for (int i = 1; i <= n; i++) // montado a cada linha (inclui poucos pontos desnecessarios, com lixo)
+    for (int iteracao = 0; iteracao < 4; iteracao++)
     {
-        // NOTE: esta montagem considera k(x)=1 e q(x)=0, fazendo com que os produtos internos dos phis sejam simples
-        double aux = (double)i;
-        double xi = aux * h;
-        double xi_ant = (aux - 1) * h;
-        double xi_prox = (aux + 1) * h;
+        int n = _n[iteracao];
+        // Montagem da matriz com produtos internos (integrais)
+        double h;
+        if (questao == 't')
+            h = 1 / ((double)n + 1);
+        else
+            h = L / ((double)n + 1); // Intervalo de [0, L] // TODO: corrigir para esse caso
+                                     // Na validacao q=0 e k=1
+                                     // ~f = f +(b-a)*k' - q*(a + (b-a)*x)
 
-        // Para correcao dos intervalos define-se a seguinte consideracao (utilizada localmente)
-        // double _xi = xi / L;
-
-        // Construcao da matriz A
-        
-        // b[i] = 2 / h;
-        // c[i] = -1 / h;
-        // a[i + 1] = c[i];
-        // d[i] = calcula_integral(xi_ant, xi_prox, T); // NOTE: Essa funcao de integracao esta para f*phi (para outros produtos internos deve-se ter outra funcao)
-
-        b[i] = integra(xi_ant / L, xi / L, -1, h, &produto_phis_normalizados) + integra(xi, xi_prox, -1, h, &produto_phis_normalizados); // TODO: conferir se so' o primeiro intervalo de integracao e' normalizado
-        c[i] = a[i + 1] = integra(xi / L, xi_prox / L, -1, h, &produto_phis_normalizados_variacao);
-        d[i] = integra(xi_ant, xi, xi, h, &produto_phi_f) + integra(xi, xi_prox, xi, h, &produto_phi_f);
-    }
-
-    // Opcional, mostrar variaveis construidas
-    if (debug)
-    {
-        imprimir_vetor(a, n + 1, 1);
-        cout << "-------" << endl;
-        imprimir_vetor(b, n, 1);
-        cout << "-------" << endl;
-        imprimir_vetor(c, n, 1);
-        cout << "-------" << endl;
-        imprimir_vetor(d, n, 1);
-        cout << "-------" << endl;
-    }
-
-    // Decomposicao LU
-    decomposicaoLU(a, b, c, l, u, n);
-
-    // Solucao da matriz tridiagonal
-    solucaoLU(x, y, d, l, u, c, n);
-    // vetor x é o vetor de alfas
-    if (debug)
-    {
-        cout << "-------" << endl;
-        imprimir_vetor(x, n, 1);
-        cout << "-------" << endl;
-    }
-
-    // Resultado exato e comparacoes
-    for (int i = 1; i <= n; i++)
-    {
-        double v_barra = 0.0;
-        double aux_i = (double)i;
-        double xi = aux_i * h;
-        for (int j = 1; j <= n; j++)
+        for (int i = 1; i <= n; i++) // montado a cada linha (inclui poucos pontos desnecessarios, com lixo)
         {
-            // TODO: reavaliar
-            double aux_j = (double)j;
-            double xj = aux_j * h;
-            v_barra += x[j] * phi_i(h, xi, xj - h, xj, xj + h);
-        }
-        // Normalizando
-        double u_barra = v_barra + fa + ((fb - fa) * xi / L);
-        double u_exato = solucao_exata(xi, fa, fb);
+            // NOTE: esta montagem considera k(x)=1 e q(x)=0, fazendo com que os produtos internos dos phis sejam simples
+            double aux = (double)i;
+            double xi = aux * h;
+            double xi_ant = (aux - 1) * h;
+            double xi_prox = (aux + 1) * h;
 
-        cout << "u" << i << ": encontrado=" << u_barra << "; exato=" << u_exato << "\n";
+            // Para correcao dos intervalos define-se a seguinte consideracao (utilizada localmente)
+            // double _xi = xi / L;
+
+            // Construcao da matriz A
+
+            // b[i] = 2 / h;
+            // c[i] = -1 / h;
+            // a[i + 1] = c[i];
+            // d[i] = calcula_integral(xi_ant, xi_prox, T); // NOTE: Essa funcao de integracao esta para f*phi (para outros produtos internos deve-se ter outra funcao)
+
+            b[i] = integra(xi_ant / L, xi / L, -1, h, &produto_phis_normalizados) + integra(xi, xi_prox, -1, h, &produto_phis_normalizados); // TODO: conferir se so' o primeiro intervalo de integracao e' normalizado
+            c[i] = a[i + 1] = integra(xi / L, xi_prox / L, -1, h, &produto_phis_normalizados_variacao);
+            d[i] = integra(xi_ant, xi, xi, h, &produto_phi_f) + integra(xi, xi_prox, xi, h, &produto_phi_f);
+        }
+
+        // Opcional, mostrar variaveis construidas
+        if (debug)
+        {
+            imprimir_vetor(a, n + 1, 1);
+            cout << "-------" << endl;
+            imprimir_vetor(b, n, 1);
+            cout << "-------" << endl;
+            imprimir_vetor(c, n, 1);
+            cout << "-------" << endl;
+            imprimir_vetor(d, n, 1);
+            cout << "-------" << endl;
+        }
+
+        // Decomposicao LU
+        decomposicaoLU(a, b, c, l, u, n);
+
+        // Solucao da matriz tridiagonal
+        solucaoLU(x, y, d, l, u, c, n);
+        // vetor x é o vetor de alfas
+        if (debug)
+        {
+            cout << "-------" << endl;
+            imprimir_vetor(x, n, 1);
+            cout << "-------" << endl;
+        }
+
+        // Resultado exato e comparacoes
+        double erro_max = 0.0; //||u_barra - u||, pegando o maximo
+        for (int i = 1; i <= n; i++)
+        {
+            double v_barra = 0.0;
+            double aux_i = (double)i;
+            double xi = aux_i * h;
+            for (int j = 1; j <= n; j++)
+            {
+                double aux_j = (double)j;
+                double xj = aux_j * h;
+                v_barra += x[j] * phi_i(h, xi, xj - h, xj, xj + h);
+            }
+            // Normalizando
+            double u_barra = v_barra + fa + ((fb - fa) * xi / L);
+            double u_exato = solucao_exata(xi, fa, fb);
+
+            if (debug)
+            {
+                cout << "\nPara x=" << xi << " :  ";
+                cout << "u" << i << ": encontrado=" << u_barra << "; exato=" << u_exato << "\n";
+            }
+
+            erro_max = max(erro_max, u_barra - u_exato);
+        }
+        cout << "\nErro maximo para n=" << n << " : " << erro_max;
     }
 
     // Finalizar
@@ -165,25 +172,6 @@ double integra(double a, double b, double xi, double h, double (*func)(double, d
         else
             x = a + ba2 * (T + 1);
         integral += func(x, xi, h);
-    }
-    integral = integral * ba2; // valor final
-    return integral;
-}
-
-// Quadratura Gaussiana de 2 pontos PARA PHI*F
-double integra_phi_f(double a, double b, double xi, double h)
-{
-    double integral = 0;      // valor da integral
-    double ba2 = (b - a) / 2; // valor medio do intervalo ab
-    double T = 1 / sqrt(3);   // Abcissa 2 pontos (uma negativa e outra positiva)
-    double x, y;
-    for (int i = 1; i <= 2; i++) // 2 Pontos
-    {
-        if (i == 1)
-            x = a + ba2 * (-T + 1);
-        else
-            x = a + ba2 * (T + 1);
-        integral += phi_i(h, x, xi-h, xi, xi+h) * funcao_escolhida(x, fa, fb);
     }
     integral = integral * ba2; // valor final
     return integral;
@@ -223,7 +211,7 @@ double k(double x)
 
 double produto_phi_f(double x, double xi, double h)
 {
-    return phi_i(h, x, xi-h, xi, xi+h) * funcao_escolhida(x, fa, fb);
+    return phi_i(h, x, xi - h, xi, xi + h) * funcao_escolhida(x, fa, fb);
 }
 
 double produto_phis_normalizados(double x, double xi, double h)
@@ -253,7 +241,7 @@ double funcao_escolhida(double x, double fronteira_a, double fronteira_b)
         return (12 * x * (1 - x)) - 2;
     case '1': // Para condicoes de fronteira nao homogeneas
         y = (12 * x * (1 - x)) - 2;
-        return y + fronteira_a + (fronteira_b - fronteira_a) * x;
+        return y + fronteira_a + ((fronteira_b - fronteira_a) * x / L);
     case 't': // TESTE
         return x;
     default:
